9672c4d9f6b5a32c94cdbdf8a7821623f45f34c4
blue3wiz <blue3wiz@gmail.com>
2019-05-17 05:13:19 +0900

    addd LICENSE.txt

--- build_dist.ps1
+++ build_dist.ps1
@@ -4,5 +4,6 @@ npm run-script build
 nexe out/smsgen.js -r src/*.yaml -r src/*.xml -o smsgen.exe
 mkdir dist -ErrorAction Ignore
 robocopy /MIR assets dist
-cp readme.md dist
+cp README.md dist
+cp LICENSE.txt dist
 cp smsgen.exe dist
