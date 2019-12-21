e201df9e20f560fa20f573b712be25cfb7ff8f72
blue3wiz <blue3wiz@gmail.com>
2019-12-21 17:52:33 +0900

    update settings

--- build.ps1
+++ build.ps1
@@ -1,11 +1,16 @@
+
 # proxy setting for nexe
 # $env:HTTPS_PROXY = ""
-
 npm run-script build
 nexe out/smsgen.js -r src/*.yaml -r src/*.xml -o smsgen.exe
 # nexe out/smsgen.js -r src/*.yaml -r src/*.xml -o smsgen.exe -b --target 10.16.0 
+
 mkdir dist -ErrorAction Ignore
 robocopy /MIR assets dist
 cp README.md dist
 cp LICENSE.txt dist
 cp smsgen.exe dist
+
+mv dist smsgen
+Compress-Archive -Force smsgen smsgen.zip
+mv smsgen dist
